42f3f3f30965adcce88d6c773dba46cfa20c161c
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-06-17 23:29:32 +0900

    Update omni_pocketalk.mk

--- omni_pocketalk.mk
+++ omni_pocketalk.mk
@@ -7,22 +7,20 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, device/alps/pocketalk/device.mk)
 
-$(call inherit-product, device/malata/ta2cm8/device.mk)
+PRODUCT_DEVICE := pocketalk
+PRODUCT_NAME := omni_pocketalk
+PRODUCT_BRAND := Pocketalk
+PRODUCT_MODEL := W1P
+PRODUCT_MANUFACTURER := alps
 
-PRODUCT_DEVICE := 
-PRODUCT_NAME := omni_ta2cm8
-PRODUCT_BRAND := onkyo
-PRODUCT_MODEL := TA2C-M8
-PRODUCT_MANUFACTURER := malata
-
-PRODUCT_GMS_CLIENTID_BASE := android-malata
+PRODUCT_GMS_CLIENTID_BASE := android-alps
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="full_j1009-user 8.1.0 O11019 1631080370 test-keys"
+    PRIVATE_BUILD_DESC="full_k39tv1_bsp-user 8.1.0 O11019 1548142755 test-keys"
 
-BUILD_FINGERPRINT := alps/full_j1009/j1009:8.1.0/O11019/1631080370:user/test-keys
+BUILD_FINGERPRINT := alps/full_k39tv1_bsp/k39tv1_bsp:8.1.0/O11019/1548142755:user/test-keys
